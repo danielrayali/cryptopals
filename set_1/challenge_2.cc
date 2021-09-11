d0745ae9a2353a8a5e4b3c8dbfa32876be0b0e5a
Daniel Ali <daniel.ray.ali@gmail.com>
2021-09-10 20:57:35 -0400

    Challenge 2 complete

--- set_1/challenge_2.cc
+++ set_1/challenge_2.cc
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <string>
 
+#include "xor.h"
+#include "hex_encoder.h"
+
 int main(int, char*[]) {
     std::string left("1c0111001f010100061a024b53535009181c");
     std::string right("686974207468652062756c6c277320657965");
     std::string expected("746865206b696420646f6e277420706c6179");
 
+    HexEncoder hex_encoder;
+    std::vector<uint8_t> left_bytes = hex_encoder.ToBytes(left);
+    std::vector<uint8_t> right_bytes = hex_encoder.ToBytes(right);
+    std::vector<uint8_t> xored_bytes = Xor(left_bytes, right_bytes);
+
+    std::string xored = hex_encoder.ToString(xored_bytes);
+    std::cout << "left:     " << left << std::endl;
+    std::cout << "right:    " << right << std::endl;
+    std::cout << "expected: " << expected << std::endl;
+    std::cout << "result:   " << xored << std::endl;
+
+    if (expected != xored) {
+        std::cerr << "ERROR" << std::endl;
+        return 1;
+    }
 
     return 0;
 }
--- set_1/hex_encoder.h
+++ set_1/hex_encoder.h
@@ -2,16 +2,26 @@
 
 #include <map>
 #include <vector>
+#include <sstream>
+#include <iomanip>
 
 class HexEncoder {
 public:
     HexEncoder() {
         for (char number = '0'; number <= '9'; ++number) {
-            hex_table_.emplace(number, 0 + (number - '0'));
+            hex_to_bin_table_.emplace(number, (number - '0'));
         }
         for (char letter = 'a'; letter <= 'f'; ++letter) {
-            hex_table_.emplace(letter, 10 + (letter - 'a'));
+            hex_to_bin_table_.emplace(letter, 10 + (letter - 'a'));
         }
+    }
+
+    std::string ToString(const std::vector<uint8_t>& bytes) const {
+        std::stringstream output;
+        for (auto byte : bytes) {
+            output << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte);
+        }
+        return output.str();
     }
 
     // input string:        "49276d"
@@ -29,13 +39,13 @@ public:
 
 private:
     // TODO: Is there a better data structure, e.g. array with offsets?
-    std::map<char, uint8_t> hex_table_;
+    std::map<char, uint8_t> hex_to_bin_table_;
 
     // HexToBinary('0') returns uint8_t(0)
     // HexToBinary('9') returns uint8_t(9)
     // HexToBinary('a') returns uint8_t(10)
     // HexToBinary('A') returns uint8_t(10)
     uint8_t HexToBinary(const char hex_letter) const {
-        return hex_table_.at(hex_letter);
+        return hex_to_bin_table_.at(hex_letter);
     }
 };
